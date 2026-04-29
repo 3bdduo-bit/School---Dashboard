fcabb88e36701dee88e1c8ef427c67d003664178
3bdduo-bit <3bdduo@gmail.com>
2026-04-29 13:23:50 +0300

    sexthh

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: "/School---Dashboard/",
+  base: "/School---Dashboard/ , /School---Dashboard/ , /school-dashboard/ , /school-dashboard-vite/ , /school-dashboard-react-vite/ , /school-dashboard-reactjs/ , /school-dashboard-react-js/ , /school-dashboard-reactjs-vite/ , /school-dashboard-react-js-vite/ , /school-dashboard-reactjs-vite/ , /school-dashboard-react-js-vite/ , /school-dashboard-reactjs-vite/ , /school-dashboard-react-js-vite/",
 });
